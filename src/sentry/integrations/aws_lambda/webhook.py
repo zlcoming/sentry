6d932e658336576571fb3fc1e0b0114c4cd76b6b
Stephen Cefali <scefali@sentry.io>
2020-08-04 14:14:14 -0700

    get endpoint

--- src/sentry/integrations/aws_lambda/webhook.py
+++ src/sentry/integrations/aws_lambda/webhook.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from sentry import http, options
 from sentry.api.base import Endpoint
+from sentry.models import Project, ProjectKey
+from sentry.utils.http import absolute_uri
 from sentry.utils.strings import gunzip
 from sentry.web.decorators import transaction_start
 
@@ -35,8 +37,8 @@ from sentry.web.decorators import transaction_start
 #   ]
 # }
 
-SENTRY_DSN = "https://f143df50c3f5433d88b574f7199b715d@scefali.ngrok.io/25"
-ENDPOINT = "https://scefali.ngrok.io/api/25/store/?sentry_key=f143df50c3f5433d88b574f7199b715d"
+# hard coded to the first org you have (should work in a dev environment)
+ORG_ID = 1
 
 
 class AwsLambdaWebhookEndpoint(Endpoint):
@@ -51,6 +53,14 @@ class AwsLambdaWebhookEndpoint(Endpoint):
     @transaction_start("AwsLambdaWebhookEndpoint")
     def post(self, request):
         print(request.body)
+
+        # TODO: differnet way of getting the projefct id
+        project = Project.objects.filter(organization_id=ORG_ID).first()
+        project_key = ProjectKey.get_default(project=project)
+        endpoint = absolute_uri("/api/%d/store/?sentry_key=%s"%(project.id, project_key.public_key))
+        print("endpoint", endpoint)
+
+
         for record in request.data["records"]:
             # decode and un-gzip data
             data = gunzip(b64decode(record["data"]))
