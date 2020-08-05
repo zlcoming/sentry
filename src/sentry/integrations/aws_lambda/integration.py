5d7172deb6858fbab5ed1e32859808fb02300c7f
Stephen Cefali <scefali@sentry.io>
2020-08-05 11:37:48 -0700

    remove more hardcoded things

--- src/sentry/integrations/aws_lambda/integration.py
+++ src/sentry/integrations/aws_lambda/integration.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import logging
-import time
+import uuid
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -43,6 +43,25 @@ metadata = IntegrationMetadata(
     aspects={},
 )
 
+# Taken from: https://gist.github.com/gene1wood/5299969edc4ef21d8efcfea52158dd40
+def parse_arn(arn):
+    # http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
+    elements = arn.split(":", 5)
+    result = {
+        "arn": elements[0],
+        "partition": elements[1],
+        "service": elements[2],
+        "region": elements[3],
+        "account": elements[4],
+        "resource": elements[5],
+        "resource_type": None,
+    }
+    if "/" in result["resource"]:
+        result["resource_type"], result["resource"] = result["resource"].split("/", 1)
+    elif ":" in result["resource"]:
+        result["resource_type"], result["resource"] = result["resource"].split(":", 1)
+    return result
+
 
 class AwsLambdaIntegration(IntegrationInstallation):
     pass
@@ -61,33 +80,42 @@ class AwsLambdaIntegrationProvider(IntegrationProvider):
 
     def build_integration(self, state):
         # TODO: unhardcode
-        account_id = "599817902985"
-        region = "us-east-2"
         integration_name = "Serverless Hack Bootstrap"
+
+        arn = state["arn"]
+        parsed_arn = parse_arn(arn)
+        account_id = parsed_arn["account"]
+
         integration = {
             "name": integration_name,
-            "external_id": account_id, # we might want the region as part of the external id
-            "metadata": {
-                "arn": state["arn"],
-                "region": region,
-                "account_id": account_id
-            }
+            "external_id": account_id,  # we might want the region as part of the external id
+            "metadata": {"arn": state["arn"],},
         }
         return integration
 
 
 class AwsLambdaPipelineView(PipelineView):
     def dispatch(self, request, pipeline):
+        # TODO: validate ARN
         if request.method == "POST":
             arn = request.POST["arn"]
             pipeline.bind_state("arn", arn)
             print("arn", arn)
             return pipeline.next_step()
 
+        template_url = (
+            "https://sentry-cf-stack-template.s3-us-west-2.amazonaws.com/sentryCFStackFilter.json"
+        )
+        # may need to match the format that
+        external_id = uuid.uuid4()
+        cloudformation_url = (
+            "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?"
+            "stackName=Sentry-Monitoring-Stack-Filter&templateURL=%s&param_ExternalId=%s"
+            % (template_url, external_id)
+        )
+
         return render_to_response(
             template="sentry/integrations/aws-lambda-setup.html",
             request=request,
-            context={
-                "cloudformation_url": "google.com"
-            }
+            context={"cloudformation_url": cloudformation_url},
         )
