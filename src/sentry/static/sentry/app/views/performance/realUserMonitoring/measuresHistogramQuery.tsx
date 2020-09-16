9b743e1f8194efc30ab9a78a35d8352d79caf2f6
Alberto Leal <mail4alberto@gmail.com>
2020-09-16 02:18:07 -0400

    i have no idea how this works

--- src/sentry/static/sentry/app/views/performance/realUserMonitoring/measuresHistogramQuery.tsx
+++ src/sentry/static/sentry/app/views/performance/realUserMonitoring/measuresHistogramQuery.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import {Location} from 'history';
+import isEqual from 'lodash/isEqual';
+import pick from 'lodash/pick';
 
 import {Organization} from 'app/types';
+import AsyncComponent from 'app/components/asyncComponent';
 import EventView from 'app/utils/discover/eventView';
 
 import {NUM_BUCKETS} from './constants';
 import {HistogramData} from './types';
+
+const QUERY_KEYS = [
+  'environment',
+  'project',
+  'query',
+  'start',
+  'end',
+  'statsPeriod',
+] as const;
 
 type ChildrenProps = {
   isLoading: boolean;
@@ -13,7 +25,7 @@ type ChildrenProps = {
   histogram: Partial<Record<string, HistogramData[]>>;
 };
 
-type Props = {
+type Props = AsyncComponent['props'] & {
   location: Location;
   organization: Organization;
   eventView: EventView;
@@ -24,15 +36,77 @@ type Props = {
   children: (props: ChildrenProps) => React.ReactNode;
 };
 
+type ApiResult = {
+  histogram_transaction_duration_15: number;
+  count: number;
+};
+
+type State = AsyncComponent['state'] & {
+  chartData: {data: ApiResult[]} | null;
+  zoomError?: boolean;
+};
+
 /**
  * This class is a stub for the measurements data. It simply generates some
  * random data for the time being. It should be replaced with queries that
  * retrieve the true data from the backend.
  */
-class MeasuresQuery extends React.Component<Props> {
+class MeasuresQuery extends AsyncComponent<Props, State> {
+  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
+    const {organization, location, measures} = this.props;
+
+    const selectMeasures = measures.map(name => {
+      return `histogram(${name},${NUM_BUCKETS})`;
+    });
+
+    let orderBy = 'count';
+
+    if (measures.length > 0) {
+      orderBy = `histogram_${measures[0].replace('.', '_')}_50`;
+    }
+
+    const eventView = EventView.fromSavedQuery({
+      ...this.props.eventView.toNewQuery(),
+
+      id: '',
+      name: '',
+      version: 2,
+      fields: [selectMeasures[0], 'count()'],
+      orderby: orderBy,
+      query: '',
+    });
+    const apiPayload = eventView.getEventsAPIPayload(location);
+    apiPayload.referrer = 'api.performance.latencychart';
+
+    return [
+      [
+        'histograms',
+        `/organizations/${organization.slug}/eventsv2/`,
+        {query: apiPayload},
+      ],
+    ];
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.shouldRefetchData(prevProps)) {
+      this.fetchData();
+    }
+  }
+
+  shouldRefetchData(prevProps: Props) {
+    if (this.state.loading) {
+      return false;
+    }
+    return !isEqual(pick(prevProps, QUERY_KEYS), pick(this.props, QUERY_KEYS));
+  }
+
   getHistograms(_x) {
     const {measures} = this.props;
     const {min, max} = this.props;
+
+    // console.log('measures', measures);
+    // console.log('max', max);
+    // console.log('min', min);
 
     return measures.reduce((histogram, measure) => {
       histogram[measure] = Array(NUM_BUCKETS)
